e27bfcc8ed85eea4be75c213b10c733a493ba071
jahnhtl <philipp.jahn@htlwien10.at>
2025-10-20 20:16:29 +0200

    description translated to english

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 /*
- * Author: Philipp Jahn
- * Date:   20.10.2025
- * File:   main.cpp
- * Description: Ein Programm liest fortlaufend ganze Zahlen ein. Die Eingabe endet, wenn der Nutzer die Zahl 0 eingibt. Danach soll die Summe aller eingegebenen Zahlen (ohne den abschließenden 0er) ausgegeben werden.
-   Beende automatisch, wenn bereits 20 Zahlen eingegeben wurden, und gib eine Warnung "Maximale Anzahl erreicht" und die Summe aus.
-*/
+ * Author:      Philipp Jahn
+ * Date:        20.10.2025
+ * File:        main.cpp
+ * Description: A program continuously reads whole numbers (integers) from the user. Input ends when the user enters
+ *              the number 0. After that, the program should output the sum of all entered numbers.
+ *              If 20 numbers have already been entered, the program should automatically terminate and display the
+ *              warning "Maximum number of inputs reached" along with the sum.
+ */
 
 #include <Arduino.h>
 
@@ -22,7 +24,7 @@ void loop() {
   Serial.println("\n==== Start new calculation ====");
 
   do {
-    delay(1); // without delay flushing will not work properly
+    delay(1); // without delay flushing will not work properly - data might not have arrived completely
     while(Serial.available()) Serial.read(); // flush serial buffer
 
     Serial.print("Input number: ");
@@ -35,7 +37,7 @@ void loop() {
     sum = sum + number;
 
     if (count >= MAX_COUNT) {
-      Serial.println("\nMaximum of numbers reached!");
+      Serial.println("\nMaximum number of inputs reached!");
       break; // jump out of the loop
     }   
   } while(number != 0);
