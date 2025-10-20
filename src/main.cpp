691b2df9098e316209137f4f3de8b97a6988625d
jahnhtl <philipp.jahn@htlwien10.at>
2025-10-20 16:48:28 +0200

    implemented condiction in for statement

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
-#include <Arduino.h>
+/*
+ * Author: Philipp Jahn
+ * Date:   20.10.2025
+ * File:   main.cpp
+ * Description: Ein Programm liest fortlaufend ganze Zahlen ein. Die Eingabe endet, wenn der Nutzer die Zahl 0 eingibt. Danach soll die Summe aller eingegebenen Zahlen (ohne den abschließenden 0er) ausgegeben werden.
+   Beende automatisch, wenn bereits 20 Zahlen eingegeben wurden, und gib eine Warnung "Maximale Anzahl erreicht" und die Summe aus.
+*/
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  int summe = 0;
+  int anzahl;
+  int zahl = 1; // set to 1 to avoid loop break during initial iteration
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for(anzahl = 0; anzahl <= 20 || zahl == 0; anzahl++)
+  {
+    Serial.print("Zahl eingeben: ");
+    zahl = Serial.parseInt();
+    summe = summe + zahl;
+  }
+
+  if (anzahl >= 20)
+  {
+    Serial.println("Maximal erreicht!");
+  }   
+
+  Serial.print("Summe = ");
+  Serial.println(summe);
+
 }
